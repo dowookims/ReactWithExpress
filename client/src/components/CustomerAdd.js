b849109419b72efc0d8ba856722e594dbd94dea6
dowookims <kaei2574@gmail.com>
2019-05-08 22:37:56 +0900

    Add From to save User data on MySQL and AWS RDS

---
+++ client/src/components/CustomerAdd.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import { post } from 'axios';
+
+class CustomerAdd extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      file: null,
+      name: '',
+      birthday: '',
+      gender: '',
+      job: '',
+      fileName: ''
+    }
+  }
+
+  handleFormSubmit = e => {
+    e.preventDefault();
+    this.addCustomer()
+      .then(res => console.log(res.data))
+    this.setState({
+      file: null,
+      name: null,
+      birthday: '',
+      gender: '',
+      job: '',
+      fileName: ''
+    })
+    window.location.reload();
+  }
+  
+
+  handleFileChange = e => {
+    this.setState({
+      file: e.target.files[0],
+      fileName: e.target.value
+    });
+  }
+
+  handleValueChange = e => {
+    let nextState = {};
+    nextState[e.target.name] = e.target.value;
+    this.setState(nextState)
+  }
+
+  addCustomer = () => {
+    const url = 'api/customers';
+    const formData = new FormData();
+    formData.append('image', this.state.file)
+    formData.append('name', this.state.name)
+    formData.append('birthday', this.state.birthday)
+    formData.append('gender', this.state.gender)
+    formData.append('job', this.state.job)
+    const config = {
+      headers: {
+        'content-type':'multipart/form-data'
+      }
+    }
+    return post(url, formData, config)
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleFormSubmit}>
+        <h1>고객 추가</h1>
+        프로필 이미지: <input type="file" name="file" file={this.state.file} value={this.state.fileName} onChange={this.handleFileChange} /><br />
+        이름 : <input type="text" name="name" value={this.state.name} onChange={this.handleValueChange}/><br/>
+        생년월일 : <input type="text" name="birthday" value={this.state.birthday} onChange={this.handleValueChange}/><br/>
+        성별 : <input type="text" name="gender" value={this.state.gender} onChange={this.handleValueChange}/><br/>
+        직업 : <input type="text" name="job" value={this.state.job} onChange={this.handleValueChange}/><br/>
+        <button type="submit">추가하기</button>
+      </form>
+    )
+  }
+}
+
+export default CustomerAdd
--- server.js
+++ server.js
@@ -10,6 +10,9 @@ const data = fs.readFileSync('./database.json');
 const conf = JSON.parse(data);
 const mysql = require('mysql');
 
+const multer = require('multer');
+const upload = multer({dest: './upload'})
+
 const connection = mysql.createConnection({
   host: conf.host,
   user: conf.user,
@@ -27,6 +30,22 @@ app.get('/api/customers', (req, res) => {
       res.send(rows);
     }
   )
+});
+
+app.use('/image', express.static('./upload'));
+
+app.post('/api/customers', upload.single('image'), (req, res) => {
+  let sql = 'INSERT INTO CUSTOMER VALUES (null, ?, ?, ?, ?, ?)';
+  let image = '/image/' + req.file.filename;
+  let name = req.body.name;
+  let birthday = req.body.birthday;
+  let gender = req.body.gender;
+  let job = req.body.job;
+  let params = [image, name, birthday, gender, job];
+  connection.query(sql, params,
+    (err, rows, fileds) => {
+      res.send(rows)
+    })
 });
 
 app.listen(port, () => console.log(`Listening on port: ${port}`));
